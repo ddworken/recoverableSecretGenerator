0ca6031b4ab5ec9977df24363d78d9fd3deec13f
David Dworken <david@daviddworken.com>
2014-10-21 20:28:59 -0400

    Create fileToArray.py

---
+++ fileToArray.py
@@ -0,0 +1,11 @@
+#!/bin/python
+#A crappy program to convert a text file  delimited by spaces to an array for python. 
+#DOES NOT DO ANY ERROR CHECKING!
+import csv
+csv.QUOTE_ALL=True
+filename = raw_input("Name of file containing array data separated by spaces: ")
+arrayname = raw_input("What is the name of the array: ")
+with open(filename, 'rb') as i:
+       reader = csv.reader(i)
+       for row in reader:
+               print arrayname + " = " + str(row).replace(' ', '\',\'')
