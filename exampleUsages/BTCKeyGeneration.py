3c56905bc6fd9a6db7a26777c435b24b70ccdd37
David Dworken <david@daviddworken.com>
2014-10-25 20:10:17 -0400

    Moved reused methods to either mainLib.py or BTCLib.py in the library directory. No change in behavior.

--- exampleUsages/BTCKeyGeneration.py
+++ exampleUsages/BTCKeyGeneration.py
@@ -1,153 +1,28 @@
 #!/bin/python
 import hashlib
-import Levenshtein
-import binascii
-import ecdsa
-
-CURVE_TYPE = ecdsa.curves.SECP256k1
-BASE_58_CHARS = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
-BASE_58_CHARS_LEN = len(BASE_58_CHARS)
-MAINNET_PREFIX = '80'
-
-###BTC Generation Library Start: Github Gist=https://gist.github.com/stequald/7e3b15c904e309dec646
-
-def numToWIF(numPriv):
-    privKeyHex = MAINNET_PREFIX+hex(numPriv)[2:].strip('L').zfill(64)
-    privKeySHA256Hash = hashlib.sha256(binascii.unhexlify(privKeyHex)).hexdigest()
-    privKeyDoubleSHA256Hash = hashlib.sha256(binascii.unhexlify(privKeySHA256Hash)).hexdigest()
-    checksum = privKeyDoubleSHA256Hash[:8]
-    wifNum = int(privKeyHex + checksum, 16)
-
-    # convert number to base58
-    base58CharList = []
-    for i in range(100):
-        base58CharList.append(BASE_58_CHARS[wifNum/(BASE_58_CHARS_LEN**i)%BASE_58_CHARS_LEN])
-
-    # convert character list to string, reverse string, and strip extra leading 1's
-    return ''.join(base58CharList)[::-1].lstrip('1')
-
-
-def WIFToNum(wifPriv):
-    numPriv = 0
-    for i in range(len(wifPriv)):
-        numPriv += BASE_58_CHARS.index(wifPriv[::-1][i])*(BASE_58_CHARS_LEN**i)
-
-    numPriv = numPriv/(2**32)%(2**256)
-    return numPriv
-
-
-def isValidWIF(wifPriv):
-    return numToWIF(WIFToNum(wifPriv)) == wifPriv
-
-def numToAddress(numPriv):
-    pko = ecdsa.SigningKey.from_secret_exponent(numPriv, CURVE_TYPE)
-    pubkey = binascii.hexlify(pko.get_verifying_key().to_string())
-    pubkeySHA256Hash = hashlib.sha256(binascii.unhexlify('04' + pubkey)).hexdigest()
-    pubkeySHA256RIPEMD160Hash = hashlib.new('ripemd160', binascii.unhexlify(pubkeySHA256Hash)).hexdigest()
-
-    hash1 = hashlib.sha256(binascii.unhexlify('00' + pubkeySHA256RIPEMD160Hash)).hexdigest()
-    hash2 = hashlib.sha256(binascii.unhexlify(hash1)).hexdigest()
-    checksum = hash2[:8]
-
-    encodedPubKeyHex = pubkeySHA256RIPEMD160Hash + checksum
-    encodedPubKeyNum = int(encodedPubKeyHex, 16)
-
-    base58CharIndexList = []
-    while encodedPubKeyNum != 0:
-        base58CharIndexList.append(encodedPubKeyNum % BASE_58_CHARS_LEN)
-        encodedPubKeyNum /= BASE_58_CHARS_LEN
-
-    m = 0
-    while encodedPubKeyHex[0 + m : 2 + m] == '00':
-        base58CharIndexList.append(0);
-        m = m + 2;
-
-    address = ''
-    for i in base58CharIndexList:
-        address = BASE_58_CHARS[i] + address
-
-    return '1' + address
-
-###BTC Generation Library Stop
-
-def numToPubKey(numPriv):
-    pko = ecdsa.SigningKey.from_secret_exponent(numPriv, CURVE_TYPE)
-    pubkey = binascii.hexlify(pko.get_verifying_key().to_string())
-    pubkeySHA256Hash = hashlib.sha256(binascii.unhexlify('04' + pubkey)).hexdigest()
-    pubkeySHA256RIPEMD160Hash = hashlib.new('ripemd160', binascii.unhexlify(pubkeySHA256Hash)).hexdigest()
-
-    hash1 = hashlib.sha256(binascii.unhexlify('00' + pubkeySHA256RIPEMD160Hash)).hexdigest()
-    hash2 = hashlib.sha256(binascii.unhexlify(hash1)).hexdigest()
-    checksum = hash2[:8]
-
-    encodedPubKeyHex = pubkeySHA256RIPEMD160Hash + checksum
-    return encodedPubKeyHex
-
-def containsBadWords(password):
-    passwordList = password.split(" ")
-    for i in range(0, len(passwordList)):
-        if passwordList[i] not in wordList:
-            #print passwordList[i] + " is not in the word list!"
-            return True
-    return False
-
-def fixPasswordWithBadWords(password):
-    bestMatch = ""
-    bestMatchDistance = 0
-    passwordList = password.split(" ")
-    for i in range(0, len(passwordList)):
-        for j in range(0, len(wordList)):
-            if Levenshtein.ratio(passwordList[i], wordList[j]) > bestMatchDistance:
-                bestMatch = wordList[j]
-                bestMatchDistance = Levenshtein.ratio(passwordList[i], wordList[j])
-                #print "Set bestMatch to " + wordList[j]
-                #print "set bestMatchDistance to " + str(bestMatchDistance)
-        #print passwordList[i] + " was corrected to " + bestMatch
-        passwordList[i] =  bestMatch
-        bestMatch = ""
-        bestMatchDistance = 0
-    passwordList.sort()
-    password = ' '.join(passwordList)
-    return password
-
-def sortPasswordString(password):
-    passwordList = password.split(" ")
-    passwordList.sort()
-    password = ' '.join(passwordList)
-    return password
-
-def genWordChecksum(password):
-    sum = 0;
-    passwordList = password.split(" ")
-    for i in range(0, len(passwordList)):
-        sum += wordList.index(passwordList[i])
-    modSum = sum % len(wordList)
-    return wordList[modSum]
-
-def recoverWord(password, checksum):
-    recoveredWord = str(wordList[int((wordList.index(checksum)+len(wordList))-wordList.index(genWordChecksum(password))) % len(wordList)])
-    return recoveredWord
+import library.mainLib
+import library.BTCLib
 
 def genKeys(password, checksum):
     password = password.lower()
-    if containsBadWords(password):
-        password = fixPasswordWithBadWords(password)
-    if containsBadWords(checksum):
-        checksum = fixPasswordWithBadWords(checksum)
+    if library.mainLib.containsBadWords(password):
+        password = library.mainLib.fixPasswordWithBadWords(password)
+    if library.mainLib.containsBadWords(checksum):
+        checksum = library.mainLib.fixPasswordWithBadWords(checksum)
         print "Your correct checksum is: " + checksum
-    if genWordChecksum(password) != checksum:
-        print "You forgot the word: " + recoverWord(password, checksum)
-        forgottenWord = recoverWord(password, checksum)
+    if library.mainLib.genWordChecksum(password) != checksum:
+        print "You forgot the word: " + library.mainLib.recoverWord(password, checksum)
+        forgottenWord = library.mainLib.recoverWord(password, checksum)
         password += " "
         password += forgottenWord
-        password = sortPasswordString(password)
+        password = library.mainLib.sortPasswordString(password)
         print "Your correct password is: " + password
     print "Checksum verified."
     num = int(hashlib.sha256(password+checksum).hexdigest(), 16)
-    privateKey = numToWIF(num)
-    publicKey = numToPubKey(WIFToNum(privateKey))
-    address = numToAddress(num)
-    assert isValidWIF(privateKey) == True
+    privateKey = library.BTCLib.numToWIF(num)
+    publicKey = library.BTCLib.numToPubKey(library.BTCLib.WIFToNum(privateKey))
+    address = library.BTCLib.numToAddress(num)
+    assert library.BTCLib.isValidWIF(privateKey) == True
     print "Private key: " + str(privateKey)
     print "Public key: " + str(publicKey)
     print "Address: " + str(address)
